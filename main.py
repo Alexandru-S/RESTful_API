b0371f92c21c2d38b58ce6e2d3a662b3b74deb82
alex sulea <alex.sulea@gmail.com>
2020-09-18 07:02:09 +0100

    removed comments in main.py

--- main.py
+++ main.py
@@ -5,12 +5,3 @@ app = web_app.create_app(config)
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-'''
-
-
-from web_app import app
-
-
-if __name__ == "__main__":
-    app.run(debug=True)'''
